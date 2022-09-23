58e7ca6de77b957239cec98d20d2f639e4b66092
harunkurt.me <harunlakodla@gmail.com>
2022-09-24 01:53:44 +0300

    added baseTCP-IP for client.cpp and change arduino files

---
+++ baseTCP_IP/cpp-test/client-arduino_mega.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <sys/types.h>
+
+
+
+#include <unistd.h>
+
+#include <string.h> //for memset 
+
+using namespace std;
+
+int main(){
+
+    int serverSock{0};
+	string s;
+	char val;
+	
+	//val="val\n";
+	
+    //strcpy(s,"val\n");
+    
+    struct sockaddr_in addr;
+    
+    serverSock = socket(AF_INET, SOCK_STREAM, 0);
+
+   
+    addr.sin_addr.s_addr = inet_addr("192.168.1.177");
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(23);
+
+    connect(serverSock,(struct sockaddr *)&addr,sizeof(addr));
+
+    cout<<"Connected to Server"<<endl;
+    char buffer[1024]={'h','e','l','l','o','/','n'};
+    
+	//while (true){
+	//write(serverSock,buffer,strlen(buffer));
+	write(serverSock,(char* )&s,sizeof(s));
+//	}
+    
+    close(serverSock);
+
+    std::cout<<"Socket closed."<<endl;
+   
+}
